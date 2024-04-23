95cd4372e6c2a63627e9f8915d74a46c952c18d9
Алексей Сердюк <aleksejserduk@Aleksejs-MacBook-Air.local>
2024-04-23 16:06:41 +0600

    refresh project

--- WheatherForecast/App/SceneDelegate.swift
+++ WheatherForecast/App/SceneDelegate.swift
@@ -8,58 +8,19 @@
 import UIKit
 import CoreData
 
-import WeatherKit
-import CoreLocation
-
-
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var rootController: UIViewController?
-    let param = UserDefaults.standard.bool(forKey: "isFirstOpenApp") // default = false
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-       // let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Constants.appGroupID)!
-//        let storeURL = containerURL.appendingPathComponent("WheatherForecast.sqlite")
-//        let description = NSPersistentStoreDescription(url: storeURL)
-
-//        let container = NSPersistentContainer(name: "WheatherForecast")
-//        container.persistentStoreDescriptions = [description]
-//        container.loadPersistentStores { NSPersistentStoreDescription, Error in
-//            print("NSP - ", NSPersistentStoreDescription)
-//            print("NSPE - ", Error ?? "no error")
-//        }
-
         window = UIWindow(windowScene: windowScene)
-
-        // Обновляем список локаций из базы
-        CoreDataManager.shared.reloadLocationList()
-
-        // Проверка на первый запуск приложения
-        if param == false {
-            // если произошел первый запуск приложения, то:
-
-            UserDefaults.standard.set(true, forKey: "isFirstOpenApp")
-            UserDefaults.standard.set([true, false, false, false], forKey: "settings") // default settings
-
-            rootController = PermissionViewController()
-        } else {
-            // если приложение уже открывали, то:
-            rootController = PageViewController()
-        }
-
-        window?.rootViewController = UINavigationController(rootViewController: rootController ?? UIViewController())
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
         window?.makeKeyAndVisible()
-
 
     }
     
-    
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        CoreDataManager.shared.saveContext()
-        }
 }
 
---
+++ WheatherForecast/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WheatherForecast
+//
+//  Created by Алексей Сердюк on 23.04.2024.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
