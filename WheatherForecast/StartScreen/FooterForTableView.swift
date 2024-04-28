b9d150846d87ab8c0fab528f9d5e9416a7d85215
Алексей Сердюк <aleksejserduk@Aleksejs-MacBook-Air.local>
2024-04-28 23:15:57 +0600

    added footer for tableview

---
+++ WheatherForecast/StartScreen/FooterForTableView.swift
@@ -0,0 +1,21 @@
+//
+//  FooterForTableView.swift
+//  WheatherForecast
+//
+//  Created by Алексей Сердюк on 28.04.2024.
+//
+
+import Foundation
+import UIKit
+
+class FooterForTableView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
